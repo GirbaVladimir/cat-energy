d819f0c34eb50bc3dfe017cdf7a2527391329e24
Vladimir <girba.vladimir@yandex.ru>
2020-09-13 14:59:43 +0300

    2nd try

--- source/js/main.js
+++ source/js/main.js
@@ -8,9 +8,7 @@ var sliderButtonBefore = document.querySelector('.slider__button--before');
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.remove('main-nav--opened');
 navMain.classList.add('main-nav--closed');
-
 map.classList.remove('contacts__map--nojs');
-
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -29,13 +27,11 @@ function initMap() {
     center: { lat: 59.938719, lng: 30.323046 },
     zoom: 14
   });
-
   if (window.matchMedia("(min-width: 768px)").matches) {
     map_google = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 59.939169, lng: 30.323336 },
       zoom: 15
     });}
-
   if (window.matchMedia("(min-width: 1440px)").matches) {
     map_google = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 59.939076, lng: 30.317815 },
@@ -46,7 +42,6 @@ function initMap() {
     url: "img/map-pin.png", // url
     scaledSize: new google.maps.Size(60, 53), // scaled size
   };
-
   if (window.matchMedia("(min-width: 768px)").matches) {
     var icon = {
       url: "img/map-pin.png", // url
@@ -59,22 +54,17 @@ function initMap() {
       map: map_google,
       icon: icon
     });
-  }
-
-  else if (window.matchMedia("(min-width: 768px)").matches) {
+  } else if (window.matchMedia("(min-width: 768px)").matches) {
     var marker = new google.maps.Marker({
       position: { lat: 59.938063, lng: 30.323210 },
       map: map_google,
       icon: icon
     });
-  }
-
-  else if (window.matchMedia("(max-width: 767px)").matches) {
+  } else if (window.matchMedia("(max-width: 767px)").matches) {
     var marker = new google.maps.Marker({
       position: {lat:  59.938429, lng: 30.322958},
       map: map_google,
       icon: icon
     });
   }
-
 }
